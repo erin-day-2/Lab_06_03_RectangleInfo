110605842179d3adb7a5e81d6496a05d5e8aec61
erin-day-2 <daye4@mail.uc.edu>
2022-02-16 17:44:46 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+	Scanner in = new Scanner(System.in);
+    double sideOne = 0;
+    double sideTwo = 0;
+    double sideThree = 0;
+    double sideFour = 0;
+    String trash = "";
+
+    System.out.print("Please input side one: ");
+
+    }
+}
